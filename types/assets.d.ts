1083c578bcf4e62c427ced21203b4ad2978c92fc
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-02-26 10:36:38 +0100

    Add type decclaration for svg

--- types/assets.d.ts
+++ types/assets.d.ts
@@ -7,3 +7,9 @@ declare module "*.jpg" {
 	const value: string;
 	export default value;
 }
+
+declare module "*.svg" {
+	export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+	const value: string;
+	export default value;
+}
