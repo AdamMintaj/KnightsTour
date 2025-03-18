c86faa3efb4e905666447bce637ea9f16d283a89
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-03-18 10:19:22 +0100

    Adjust Checkbox styling

--- src/components/ui/Checkbox/Checkbox.styled.ts
+++ src/components/ui/Checkbox/Checkbox.styled.ts
@@ -2,14 +2,13 @@ import styled from "@emotion/styled";
 import { ReactComponent as Icon } from "assets/checkmark.svg";
 
 export const Label = styled.label`
-	width: 20px;
-	height: 20px;
+	width: 18px;
+	height: 18px;
 	display: block;
+	border: 1px solid black;
 	border-radius: 5px;
 	cursor: pointer;
 	position: relative;
-	background: #fff7f0;
-	box-shadow: inset 2px 2px 9px #a8a39e, inset -2px -2px 9px #ffffff;
 `;
 
 export const Input = styled.input`
