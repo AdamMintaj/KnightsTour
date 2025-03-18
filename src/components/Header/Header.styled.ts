6f987cfada4c06e5ce6285176d5500a3bef3c30f
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-03-18 14:56:51 +0100

    Adjust Header styling

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,14 +1,29 @@
 import styled from "@emotion/styled";
+import { mediaQuery } from "styles/responsive";
 
 export const Container = styled.header`
-	height: 82px;
 	border-bottom: 1px solid black;
+	padding-inline: 1.25rem;
+
+	@media (${mediaQuery.desktop}) {
+		padding-inline: 3rem;
+	}
+`;
+
+export const InnerContainer = styled.div`
+	height: 82px;
+	max-width: 1500px;
+	margin: 0 auto;
 	display: flex;
 	justify-content: space-between;
-	padding-right: 1rem;
 
-	@media (max-height: 500px) and (orientation: landscape) {
-		height: 15vh;
-		padding: 0 8vw;
+	@media (${mediaQuery.landscapeSmallScreen}) {
+		height: 13vh;
+		max-height: 110px;
+		justify-content: space-around;
+	}
+
+	@media (${mediaQuery.tablet}), (${mediaQuery.desktopLarge}) {
+		height: 110px;
 	}
 `;
