0d7dc472e5412acc8029ae2688c718624a434047
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-03-18 14:16:26 +0100

    Add new colors to theme provider

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,8 @@
 const theme = {
 	colors: {
-		themeOrange: "rgb(254, 95, 0)",
+		background: "rgb(255, 247, 240)",
+		orange: "rgb(254, 125, 48)",
+		orangeSaturated: "rgb(254, 95, 0)",
 	},
 };
 
--- types/emotion.d.ts
+++ types/emotion.d.ts
@@ -3,7 +3,9 @@ import "@emotion/react";
 declare module "@emotion/react" {
 	export interface Theme {
 		colors: {
-			themeOrange: string;
+			background: string;
+			orange: string;
+			orangeSaturated: string;
 		};
 	}
 }
