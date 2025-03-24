3c8d0083db1ec770f7877d4c9bffbd5582abb610
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-03-24 14:00:17 +0100

    modify useWatchGameStatus hook; it is no longer possible to continue a lost game by turning on the dragDrop cheat

--- src/hooks/useWatchGameStatus.tsx
+++ src/hooks/useWatchGameStatus.tsx
@@ -7,11 +7,14 @@ import { GameStatus } from "context/gameTypes";
  * Monitors the state of the board and counter and updates gameStatus when specified conditions are met.
  */
 const useWatchGameStatus = () => {
-  const [{ board, counter, activeCheats }, dispatch] = useGameContext();
+  const [{ board, counter, activeCheats, gameStatus }, dispatch] = useGameContext();
 
   const noGamePlayed = counter === 0;
   const isGameWon = !board.some((cell) => cell.available) && board.every((cell) => cell.closed);
-  const isGameLost = !activeCheats.dragDrop && !board.some((cell) => cell.available);
+  // checking if gameStatus is LOST in isGameLost makes it impossible to continue an already lost game
+  // by turning on the drag&drop cheat
+  const isGameLost = gameStatus === GameStatus.LOST ||
+    (!activeCheats.dragDrop && !board.some((cell) => cell.available));
 
   useEffect(() => {
     if (noGamePlayed)
