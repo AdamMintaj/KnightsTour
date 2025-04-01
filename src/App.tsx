7df8e45980864fc82906553b5b27962efa73b954
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-04-01 12:13:57 +0200

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import useUpdateStats from 'hooks/useUpdateStats';
+import { Analytics } from "@vercel/analytics/react"
 
 // TODO: create alerts system (and use it in Richard component)
+
+const disableAnalytics = localStorage.getItem('disableAnalytics') === 'true';
 
 const App = () => {
   useUpdateStats();
@@ -13,6 +16,7 @@ const App = () => {
       <Header />
       <Outlet />
       <Footer />
+      {!disableAnalytics && <Analytics />}
     </>
   );
 }
